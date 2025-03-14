6a2878f0cf2ed4159ea1f3f36e41581934290afc
kauansouzasenai <kauanbbbaria@gmail.com>
2025-03-14 12:55:41 -0300

    14-03

--- Exercicios/ex1_2.js
+++ Exercicios/ex1_2.js
@@ -5,3 +5,6 @@ prompt = prompt();
 
 const n1 = prompt("digite o primeiro número: ");
 console.log(n1);
+
+const n2 = parseFloat(prompt("numero dois!"));
+console.log("a soma de n1+n2 é : ", n1 + n2);
---
+++ helloworld.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+
+/* Método express.get necessita de dois paârametros
+na ARROW FUNCTION, o primeiro são os dados do servidor(REQUISITION - 'REQ')
+o segundo, são os dados que serão enviador para ao cliente (RESULE - "RES")
+
+*/
+app.get("/", (req, res) => {
+  res.send("Ola Sesi");
+});
+
+// app.listes() deve ser o ultimo comando da aplicação (app.js)
+app.listen(PORT, () => {
+  console.log(`Servidor sendo executado na porta ${PORT}`);
+});
